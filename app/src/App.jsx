0e0fe033308db9995a0c9f9087c73365a041bd83
Nicholas Hellmers <nihe1661@colorado.edu>
2024-01-04 22:14:30 -0300

    Added products fetch

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -6,17 +6,30 @@ const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 async function getData() {
-  const { data } = await supabase.from("test").select();
+  const { data } = await supabase.from("products").select().limit(20);
   return data;
 }
 
 function App() {
+  // const [test_data] = createResource(getData);
+
+  // limit to 3 items to 20 items
   const [test_data] = createResource(getData);
 
   return (
-    <ul>
-      <For each={test_data()}>{(entry) => <li>{entry.id} {entry.username}</li>}</For>
-    </ul>
+    <div className='m-auto py-24'>
+      <ul className='grid grid-cols-5 gap-5 md:w-[70%]'>
+        <For each={test_data()}>{(entry) => <ul id={entry.id}>
+        <img src={entry.image_url} alt={entry.name} />
+        <li>{entry.name}</li>
+        <li>{entry.price}</li>
+        <li>{entry.supermarket}</li>
+        <li><a href={entry.product_url}>Get</a></li>
+        </ul>
+        }
+        </For>
+      </ul>
+    </div>
   );
 }
 
