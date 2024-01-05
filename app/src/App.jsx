2bde3f7d53bea90c506e4cbe7a83f01f35bd46c1
Nicholas Hellmers <nihe1661@colorado.edu>
2024-01-05 13:52:01 -0300

    test deploy

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -6,29 +6,46 @@ const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 async function getData() {
-  const { data } = await supabase.from("products").select().limit(20);
+  const { data } = await supabase.from("products").select("*");
+  return data;
+}
+
+async function searchProducts(searchTerm) {
+  const { data } = await supabase.from("products").select("*").ilike("name", `%${searchTerm}%`).limit(5);
+  console.log(data);
   return data;
 }
 
 function App() {
-  // const [test_data] = createResource(getData);
-
-  // limit to 3 items to 20 items
   const [test_data] = createResource(getData);
 
   return (
-    <div className='m-auto py-24'>
-      <ul className='grid grid-cols-5 gap-5 md:w-[70%]'>
-        <For each={test_data()}>{(entry) => <ul id={entry.id}>
-        <img src={entry.image_url} alt={entry.name} />
-        <li>{entry.name}</li>
-        <li>{entry.price}</li>
-        <li>{entry.supermarket}</li>
-        <li><a href={entry.product_url}>Get</a></li>
+    <div className='py-24'>
+      {/* Generate search bar that shows drop down of search results*/}
+      <div className='flex justify-center'>
+        <input
+          type="text"
+          placeholder="Search"
+          className='border-2 border-black rounded-lg'
+          onInput={(e) => {
+            searchProducts(e.target.value);
+          }}
+        />
+      </div>
+      <div className='flex justify-center'>
+        <ul className='grid grid-cols-5 gap-5 md:w-[80%]'>
+          <For each={test_data()}>{(entry) => <ul className='border' id={entry.id}>
+            <a href={entry.product_url}>
+              {/* <img src={entry.image_url} alt={entry.name} /> */}
+              <li>{entry.name}</li>
+              <li>{entry.price}</li>
+              <li>{entry.supermarket}</li>
+            </a>
+          </ul>
+          }
+          </For>
         </ul>
-        }
-        </For>
-      </ul>
+      </div>
     </div>
   );
 }
