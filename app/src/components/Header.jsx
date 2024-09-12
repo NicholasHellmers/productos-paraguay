8ecd0334b4a88d4c16991ff2e1e9bc42999ccc2c
Nicholas Hellmers <nihe1661@colorado.edu>
2024-09-12 15:05:19 -0600

    Fixed kaggle link

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -1,12 +1,12 @@
 export default function Header() {
   return (
     <div>
-        <div className="flex justify-center">
+        <div className="flex justify-center my-5">
             <h1 className="text-4xl font-bold">Paraguay Product DB</h1>
         </div>
-        <div className="flex justify-center">
+        <div className="w-[100%] flex justify-center items-center flex-col gap-5">
             <h2 className="text-2xl font-bold">Check real products across Paraguayan ecommerce stores.</h2>
-            <h2 className="text-2xl font-bold"><a href="https://www.kaggle.com/datasets/nikihellmers/paraguay-supermarket-products" target="_blank">Click here</a> to download the latest version of the dataset.</h2>
+            <a className="text-2xl font-bold" href="https://www.kaggle.com/datasets/nikihellmers/paraguay-supermarket-products" target="_blank">Click here to download the latest version of the dataset.</a>
         </div>
 
     </div>
