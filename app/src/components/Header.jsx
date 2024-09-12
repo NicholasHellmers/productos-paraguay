6e6e44e1b81e89f4d33e28965964004065d59e88
Nicholas Hellmers <nihe1661@colorado.edu>
2024-09-12 14:59:58 -0600

    Added link for kaggle

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -6,6 +6,7 @@ export default function Header() {
         </div>
         <div className="flex justify-center">
             <h2 className="text-2xl font-bold">Check real products across Paraguayan ecommerce stores.</h2>
+            <h2 className="text-2xl font-bold"><a href="https://www.kaggle.com/datasets/nikihellmers/paraguay-supermarket-products" target="_blank">Click here</a> to download the latest version of the dataset.</h2>
         </div>
 
     </div>
